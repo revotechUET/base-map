9340d1098df816f981175f16bd1998a42c707fc5
Hung <hungnk1996@gmail.com>
2019-04-16 08:48:28 +0700

    update

--- src/base-map/index.js
+++ src/base-map/index.js
@@ -9,13 +9,61 @@ app.component(componentName, {
     controllerAs: 'self',
     bindings: {
         zoneDefault: "@",
-        hasWiLogin: "<"
+        hasWiLogin: "<",
+        projectId: "<",
+        hasProjectList: "<",
+        username: "@",
+        password: "@"
     },
     transclude: true
 });
 
 function baseMapController($scope, $http, wiToken) {
+
     let self = this;
+    $scope.wellList = [];
+    $scope.wellSelect = [];
+    $scope.focusWell = [];
+    $scope.deleteWellId = 0;
+
+    this.$onInit = function () {
+        //CHECK TOKEN
+        if ((localStorage.getItem("token")) !== null) {
+            getProjectList();
+            getZoneList();
+        }
+
+        if (self.username && self.password) {
+            $http({
+                method: 'POST',
+                url: 'http://admin.dev.i2g.cloud/login',
+                data: {
+                    username: self.username,
+                    password: self.password,
+                },
+                headers: {}
+            }).then(function (response) {
+                wiToken.setToken(response.data.content.token);
+                wiToken.saveToken(response.data.content)
+            }, function (errorResponse) {
+                console.error(errorResponse);
+            });
+        }
+        $scope.$watch(function () {
+
+            return localStorage.getItem('token');
+        }, function (newValue, oldValue) {
+            // console.log(newValue, oldValue);
+            if ((localStorage.getItem("token")) !== null) {
+                getProjectList();
+                getZoneList();
+            }
+
+        });
+    }
+
+
+    //GET ZONE FROM SERVER
     function getZoneList() {
         $http({
             method: 'POST',
@@ -41,24 +89,14 @@ function baseMapController($scope, $http, wiToken) {
         });
     }
 
-    $scope.wellList = [];
-    $scope.wellSelect = [];
-    $scope.focusWell = [];
-    $scope.deleteWellId = 0;
-
-    if ((localStorage.getItem("token")) !== null) {
-        getProjectList();
-        getZoneList();
+    $scope.getIdWell = function (wellSelectIdx) {
+        // this.baseClick.apply(this, arguments);
+        $scope.deleteWellId = $scope.wellSelect[wellSelectIdx].properties.idWell;
+        $scope.focusWell = $scope.wellSelect[wellSelectIdx].properties;
     }
-    $scope.$watch(function () {
-        return localStorage.getItem('token');
-    }, function (newValue, oldValue) {
-        // console.log(newValue, oldValue);
-        if ((localStorage.getItem("token")) !== null) {
-            getProjectList();
-            getZoneList();
-        }
-    });
+    $scope.focusWellonMap = function (wellSelectIdx) {
+        $scope.focusWell = $scope.wellSelect[wellSelectIdx].properties;
+    }
 
     this.refesh = function () {
         getProjectList();
@@ -86,43 +124,62 @@ function baseMapController($scope, $http, wiToken) {
     function getProjectList(projectList) {
         var projectList = [];
         let _token = wiToken.getToken();
-        if(!_token){
+        if (!_token) {
             window.alert("Please login!");
             return;
         }
-        $http({
-            method: 'POST',
-            url: 'http://dev.i2g.cloud/project/list',
-            data: {},
-            headers: {
-                "Authorization": wiToken.getToken(),
-            }
-        }).then(function (response) {
-            let projects = response.data.content;
-            for (let index = 0; index < projects.length; index++) {
-                projectList.push({
-                    data: {
-                        icon: "project-normal-16x16",
-                        label: projects[index].alias,
-                    },
-                    properties: projects[index]
-                });
-            }
-        }, function (errorResponse) {
-            window.alert("Unauthorized access!");
-            console.error(errorResponse);
-        });
-        $scope.projectList = projectList;
-    }
-
-    function pushWellintoList(list, wellToPush) {
-        list.push({
-            data: {
-                icon: "well-16x16",
-                label: wellToPush.name,
-            },
-            properties: wellToPush
-        });
+        if (self.projectId) {
+            this.hasProjectList = true;
+            var wellList = [];
+            $http({
+                method: 'POST',
+                url: 'http://dev.i2g.cloud/project/well/list',
+                data: {
+                    idProject: self.projectId
+                },
+                headers: {
+                    "Authorization": wiToken.getToken(),
+                }
+            }).then(function (response) {
+                let wells = response.data.content;
+                for (let index = 0; index < wells.length; index++) {
+                    pushWellintoList(wellList, wells[index]);
+                }
+                $scope.wellList = wellList;
+                if ((wellList.length) === 0) {
+                    window.alert("Well not found in this project!");
+                }
+            }, function (errorResponse) {
+                console.error(errorResponse);
+            });
+            return;
+        } else if (self.projectId === undefined) {
+            this.hasProjectList = false;
+            $http({
+                method: 'POST',
+                url: 'http://dev.i2g.cloud/project/list',
+                data: {},
+                headers: {
+                    "Authorization": wiToken.getToken(),
+                }
+            }).then(function (response) {
+                let projects = response.data.content;
+                for (let index = 0; index < projects.length; index++) {
+                    projectList.push({
+                        data: {
+                            icon: "project-normal-16x16",
+                            label: projects[index].alias,
+                        },
+                        properties: projects[index]
+                    });
+                }
+            }, function (errorResponse) {
+                window.alert("Unauthorized access!");
+                console.error(errorResponse);
+            });
+            $scope.projectList = projectList;
+            return;
+        }
     }
 
     $scope.onClickPrj = function (prjIdx) {
@@ -176,12 +233,14 @@ function baseMapController($scope, $http, wiToken) {
     this.showAllPopup = function () {
 
     }
-    $scope.getIdWell = function (wellSelectIdx) {
-        // this.baseClick.apply(this, arguments);
-        $scope.deleteWellId = $scope.wellSelect[wellSelectIdx].properties.idWell;
-        $scope.focusWell = $scope.wellSelect[wellSelectIdx].properties;
-    }
-    $scope.focusWellonMap = function (wellSelectIdx) {
-        $scope.focusWell = $scope.wellSelect[wellSelectIdx].properties;
+
+    function pushWellintoList(list, wellToPush) {
+        list.push({
+            data: {
+                icon: "well-16x16",
+                label: wellToPush.name,
+            },
+            properties: wellToPush
+        });
     }
 }
