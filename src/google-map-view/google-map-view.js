b9e33c65f5cef11eca7f3a3c4fd92adea473ba05
Nguyễn Đức Thắng <nguyenducthang.uet@gmail.com>
2019-12-10 14:06:31 +0700

    update

--- src/google-map-view/google-map-view.js
+++ src/google-map-view/google-map-view.js
@@ -12,12 +12,22 @@ app.component(componentName, {
   bindings: {
     wells: "<",
     zoneMap: "<",
+    // contour
     focusCurve: "<",
     getCurveInfoFn: "<",
     showContour: "<",
+    // view by marker or zone
+    getCurveRawDataFn: "<",
+    focusMarkerOrZone: "<",
+    zoneDepthSpec: '<'
   },
   transclude: true
 });
+
+const ZONE_DEPTH_SPEC_MAP = {
+  'zone-top': 'startDepth',
+  'zone-bottom': 'endDepth'
+}
 
 function googleMapViewController($scope, $timeout, ngDialog) {
   let self = this;
@@ -40,6 +50,8 @@ function googleMapViewController($scope, $timeout, ngDialog) {
       },
       true
     );
+
+    // CONTOUR
     $scope.$watch(
       () => self.focusCurve,
       () => {
@@ -52,6 +64,20 @@ function googleMapViewController($scope, $timeout, ngDialog) {
         updateContours();
       }
     );
+
+    // VIEW BY ZONESET & MARKERSET
+    $scope.$watch(
+      () => self.focusMarkerOrZone,
+      () => {
+        updateCoordinateTable();
+      }
+    )
+    $scope.$watch(
+      () => self.zoneDepthSpec,
+      () => {
+        updateCoordinateTable();
+      }
+    )
   };
 
  
@@ -66,6 +92,13 @@ function googleMapViewController($scope, $timeout, ngDialog) {
     let firstProjection = self.zoneMap;
     let secondProjection = "+proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees";
     console.log(self.wells)
+
+    // remove all previous markers
+    for (let marker of Object.values(markers)) {
+      marker.setMap(null);
+      delete marker;
+    }
+
     if (self.zoneMap) {
       // markers.length = 0;
       if (!(self.wells || []).length) return 0;
@@ -83,10 +116,146 @@ function googleMapViewController($scope, $timeout, ngDialog) {
         else if (checkCoordinate(lat, long, x, y) === false) {
           aMarker = new google.maps.Marker({position: {lat: latX, lng: lngY}, map: map});
         }
+        if (aMarker) {
+          markers[self.wells[index].idWell] = aMarker;
+        }
       }
     }
   }
 
+  // ===================== DRAWING BY ZONE AND MARKER SET ===================
+  let coordinateHash = {};
+  const alertDebounce = _.debounce(function(message) {
+    ngDialog.open({
+      template: "templateError",
+      className: "ngdialog-theme-default",
+      scope: Object.assign($scope.$new(), { message: message})
+    })
+  }, 1000);
+  function updateCoordinateTable() {
+    async.eachSeries(self.wells, (well, next) => {
+      getCoordFromCurve(well)
+        .then(coord => {
+          coordinateHash[well.idWell] = coord;
+          console.log(coord);
+          next();
+        })
+    }, () => {
+      drawMarkers();
+      // drawMarkersDebounced();
+    })
+  }
+  async function getCoordFromCurve(well) {
+    const focusedMZ = self.focusMarkerOrZone;
+    if (!focusedMZ) {
+      return;
+    }
+    let depth = null;
+    if (focusedMZ.idZone) {
+      const matchZoneset = well.zone_sets.find(zs => zs.name == focusedMZ.zonesetName);
+      if (matchZoneset) {
+        const matchZone = matchZoneset.zones.find(z => z.zone_template.name == focusedMZ.name);
+        if (matchZone)
+          if(self.zoneDepthSpec == 'zone-middle') {
+            depth = (matchZone.startDepth + matchZone.endDepth) / 2;
+          } else {
+            depth = matchZone[ZONE_DEPTH_SPEC_MAP[self.zoneDepthSpec || 'zone-top']];
+          }
+      }
+    } else if (focusedMZ.idMarker) {
+      const matchMarkerset = well.marker_sets.find(ms => ms.name == focusedMZ.markersetName);
+      if (matchMarkerset) {
+        const matchMarker = matchMarkerset.markers.find(m => m.marker_template.name == focusedMZ.name);
+        if (matchMarker)
+          depth = matchMarker.depth;
+      }
+    }
+    let x, y;
+    let lat, lng;
+    if (_.isFinite(depth)) {
+      const indexDataset = well.datasets.find(ds => ds.name == "INDEX");
+      if (indexDataset) {
+        const xOffsetCurve = indexDataset.curves.find(c => c.idFamily == 762)
+        const yOffsetCurve = indexDataset.curves.find(c => c.idFamily == 764)
+
+        if (xOffsetCurve && yOffsetCurve) {
+          const top = Number(indexDataset.top);
+          const step = Number(indexDataset.step);
+          const xOffsetData = await new Promise((res) => {
+            self.getCurveRawDataFn(xOffsetCurve.idCurve, (err, data) => {
+              res(data.map(d => Object.assign(d, {depth: top + step * d.y})).filter(d => _.isFinite(d.x)));
+            });
+          });
+          const yOffsetData =  await new Promise((res) => {
+            self.getCurveRawDataFn(yOffsetCurve.idCurve, (err, data) => {
+              res(data.map(d => Object.assign(d, {depth: top + step * d.y})).filter(d => _.isFinite(d.x)));
+            });
+          });
+
+          if (xOffsetData.length && yOffsetData.length) {
+            const firstProjection = self.zoneMap;
+            const secondProjection = "+proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees";
+
+            const _lat = getLat(well.well_headers, true);
+            const _lng = getLong(well.well_headers, true);
+            const _x = getX(well.well_headers, true);
+            const _y = getY(well.well_headers, true);
+
+            const xUpperBoundIdx = xOffsetData.findIndex(datum => datum.depth >= depth);
+            const xUpperBound = xOffsetData[xUpperBoundIdx];
+            const xLowerBound = xOffsetData[xUpperBoundIdx - 1];
+
+            // calculate x, y offsets
+            let _xOffset, _yOffset;
+            if (xLowerBound) 
+              _xOffset = d3.scaleLinear().domain([xLowerBound.depth, xUpperBound.depth]).range([xLowerBound.x, xUpperBound.x])(depth);
+            else
+              _xOffset = (xUpperBound || xOffsetData[xOffsetData.length - 1]).x;
+
+            const yUpperBoundIdx = yOffsetData.findIndex(datum => datum.depth  >=depth);
+            const yUpperBound = yOffsetData[yUpperBoundIdx];
+            const yLowerBound = yOffsetData[yUpperBoundIdx - 1];
+
+            if (yLowerBound)
+              _yOffset = d3.scaleLinear().domain([yLowerBound.depth, yUpperBound.depth]).range([yLowerBound.x, yUpperBound.x])(depth);
+            else
+              _yOffset = (yUpperBound || yOffsetData[yOffsetData.length - 1]).x;
+
+
+            const _checkCoordResult = checkCoordinate(_lat, _lng, _x, _y);
+            if (_checkCoordResult== true) {
+              // calculate new lat/lng, x/y from x, y offset
+              const zeroProject = proj4(firstProjection, secondProjection, [0, 0]);
+              const offsetProject = proj4(firstProjection, secondProjection, [_xOffset, _yOffset]);
+              lat = _lat + (offsetProject[1] - zeroProject[1]);
+              lng = _lng + (offsetProject[0] - zeroProject[0]);
+              const revertPrj = proj4(secondProjection, firstProjection, [lng, lat]);
+              x = revertPrj[0];
+              y = revertPrj[1];
+            } else if (_checkCoordResult == false) {
+              // calculate new lat/lng from new x, y
+              x = _x + _xOffset;
+              y = _y + _yOffset;
+              const prjResult = proj4(firstProjection, secondProjection, [x, y]);
+              lat = prjResult[1];
+              lng = prjResult[0];
+
+            }
+          }
+        } else {
+          console.warn(`Cannot find XOFFSET or YOFFSET curve in INDEX dataset of well ${well.name}`);
+        alertDebounce(`Cannot find XOFFSET or YOFFSET curve in INDEX dataset of well ${well.name}`);
+        }
+      } else {
+        console.warn(`Cannot find INDEX dataset in well ${well.name}`);
+        alertDebounce(`Cannot find INDEX dataset in well ${well.name}`);
+      }
+    }
+    return { x, y, lat, lng };
+  }
+  // ====================== END DRAWING BY ZONE AND MARKER SET ====================
+
+  // ======================= DRAWING CONTOUR ===========================
   let contour = null;
   function initContours() {
     contour = new Contour("#contour-map-container", map, []);
@@ -106,7 +275,7 @@ function googleMapViewController($scope, $timeout, ngDialog) {
       contour.data = await genContourData();
     }
   }
-  const data = [];
+  const _contourData = [];
   async function genContourData() {
     const _data = [];
     let firstProjection = self.zoneMap;
@@ -138,10 +307,10 @@ function googleMapViewController($scope, $timeout, ngDialog) {
           }
         }
       }
-      data.length = 0;
-      _data.forEach(d => data.push(d));
+      _contourData.length = 0;
+      _data.forEach(d => _contourData.push(d));
     }
-    return data;
+    return _contourData;
   }
   async function getWellDataForContour(well) {
     if (!self.focusCurve) return 0;
@@ -168,6 +337,7 @@ function googleMapViewController($scope, $timeout, ngDialog) {
     if (!curveInfo || !curveInfo.DataStatistic) return 0;
     return curveInfo.DataStatistic.meanValue;
   }
+  // =================== END DRAWING CONTOUR =========================
 
 function checkCoordinate(lat, long, x, y) {
   if ((!lat || !long) && (x && y)) {
