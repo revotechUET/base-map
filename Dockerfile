d5170f8fdb7371e033f887687bb3c7078490cc70
Quang LN <quangln2810@gmail.com>
2022-06-01 12:03:03 +0700

    fix bundle

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM nginx:1.16.0-alpine
 
-COPY dist/ /usr/share/nginx/html/dist
-COPY bower_components/ /usr/share/nginx/html/bower_components
-COPY vendors/ /usr/share/nginx/html/vendors
-COPY index.html /usr/share/nginx/html
+COPY dist/ /usr/share/nginx/html
 
 # expose port 80
 EXPOSE 80
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const HtmlWebpackDeployPlugin = require('html-webpack-deploy-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-	context: __dirname + '/src',
+	// context: __dirname + '/src',
 	mode: "development",
 	optimization: {
 		// We no not want to minimize our code.
 		minimize: false
 	},
 	entry: {
-		main: "./index.js",
+		main: "./src/index.js",
 	},
 	output: {
 		path: __dirname + '/dist',
@@ -16,30 +17,56 @@ module.exports = {
 	module: {
 		rules: [{
 			test: /\.html$/,
-			use: [{
-				loader: 'html-loader',
-				options: {
-					interpolate: true
-				}
-			}]
+			use: 'html-loader',
+			// use: [{
+			// loader: 'html-loader',
+			// options: {
+			// 	interpolate: true
+			// }
+			// }]
 		}, {
 			test: /\.css$/,
 			use: ['style-loader', 'css-loader'],
+			sideEffects: true,
 		},
 		{
 			test: /\.less$/,
 			use: ['style-loader', 'css-loader', 'less-loader'],
+			sideEffects: true,
 		},
 		{
 			test: /\.(png|jpeg|ttf|...)$/,
-			use: [
-				{ loader: 'url-loader' }
-				// limit => file.size =< 8192 bytes ? DataURI : File
-			]
+			loader: 'url-loader',
+			options: {
+				limit: 8192,
+				outputPath: 'assets',
+			},
 		}
 		],
 	},
-  plugins: [
-    new HardSourceWebpackPlugin()
-  ]
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',
+			chunksSortMode: 'none',
+		}),
+		new HtmlWebpackDeployPlugin({
+			append: false,
+			assets: {
+				copy: [
+					{ from: './vendors', to: '../vendors' },
+					{ from: './bower_components', to: '../bower_components' },
+				],
+			},
+			packages: {
+				'@revotechuet/misc-component': {
+					copy: [{ from: 'dist/misc-components.js', to: 'misc-components.js' }],
+					scripts: 'misc-components.js'
+				},
+				'@revotechuet/file-explorer': {
+					copy: [{ from: 'dist/file-explorer-module.js', to: 'file-explorer-module.js' }],
+					scripts: 'file-explorer-module.js'
+				},
+			}
+		})
+	]
 }
